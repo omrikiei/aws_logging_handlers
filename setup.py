4b7d5003b608d33dee2c81dacc6421637f57d4fe
Omri Eival <omri.eival@ibm.com>
2018-04-23 12:15:03 +0300

    modified setup.py to comply with new version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='logging-s3-handler',
     packages=['logging_s3_handler'],
-    version='0.1.1',
+    version='0.1.2',
     description='A logging handler that streams log records to Aws S3 objects',
     author='Omri Eival',
     author_email='omrieival@gmail.com',
     url='https://github.com/omrikiei/logging_s3_handler/',
-    download_url='https://github.com/omrikiei/logging_s3_handler/archive/0.1.tar.gz',
+    download_url='https://github.com/omrikiei/logging_s3_handler/archive/0.1.2.tar.gz',
     keywords=['logging', 's3', 'aws', 'handler'],
     classifiers=[
         'Development Status :: 3 - Alpha',
