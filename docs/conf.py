aaf7911776e6e6f0cf96e256cdfe8b8834afdeb3
Omri Eival <omri.eival@ibm.com>
2019-02-15 08:05:56 +0200

    added mock import

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../aws_logging_handlers/'))
+autodoc_mock_imports = ['boto3']
+
 
 
 # -- Project information -----------------------------------------------------
